87445fae2668ca3664b81e733f4128d2beaab252
Baltzar Mattsson (work) <baltzar.mattsson@wagnerwayfinder.com>
2017-10-05 23:09:40 +0200

    adding some settings and mock data

--- INFC20Backend/Controllers/BidController.cs
+++ INFC20Backend/Controllers/BidController.cs
@@ -5,6 +5,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading.Tasks;
 using System.Web.Http;
 
 namespace INFC20Backend.Controllers
@@ -12,7 +13,29 @@ namespace INFC20Backend.Controllers
     public class BidController : ApiController
     {
 
-        private BidDAL bidDal = new BidDAL(); 
+        private BidDAL bidDal = new BidDAL();
+
+        // temp
+        [HttpGet]
+        [Route("api/Bid/GetTempListings")]
+        public async Task<HttpResponseMessage> GetTempListings() {
+
+            var tempListings = new[] {
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 1", title = "Title 1" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 2", title = "Title 2" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 3", title = "Title 3" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 4", title = "Title 4" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 5", title = "Title 5" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 6", title = "Title 6" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 7", title = "Title 7" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 8", title = "Title 8" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 9", title = "Title 9" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 10", title = "Title 10" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 11", title = "Title 11" },
+                new { start_time = 0, end_time = 0, number = -1, picture = "", description = "Description 12", title = "Title 12" },
+            }.ToList();
+            return Request.CreateResponse(HttpStatusCode.OK, tempListings);
+        }
 
         // GET: api/Bid
         public IEnumerable<string> Get()
---
+++ INFC20Backend/Models/Listing.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace INFC20Backend.Models
+{
+    public class Listing
+    {
+    }
+}
